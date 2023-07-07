a10fdb406e8e162f5c74fe28b3ea1670f6a4cb9b
Faisal Memon <faisal.memon1991@gmail.com>
2023-07-07 15:26:22 +0530

    spring-micro-0840-challenge-update-job

--- firstjobapp/src/main/java/com/embarkx/firstjobapp/job/JobController.java
+++ firstjobapp/src/main/java/com/embarkx/firstjobapp/job/JobController.java
@@ -40,6 +40,15 @@ public class JobController {
             return new ResponseEntity<>("Job deleted successfully",HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+    @PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id,
+                                            @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJob(id, updatedJob);
+        if (updated)
+            return new ResponseEntity<>("Job updated successfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
 /*
 
