6ab81403231dce84f86e3ea70156cf460aad1ea9
Faisal Memon <faisal.memon1991@gmail.com>
2023-07-07 13:49:39 +0530

    spring-micro-0800-responseentity-class

--- firstjobapp/src/main/java/com/embarkx/firstjobapp/job/JobController.java
+++ firstjobapp/src/main/java/com/embarkx/firstjobapp/job/JobController.java
@@ -1,8 +1,9 @@
 package com.embarkx.firstjobapp.job;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,26 +15,24 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+        return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id){
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
         Job job = jobService.getJobById(id);
         if(job != null)
-            return job;
-        return new Job(1L, "TestJob","TestJob"
-                ,"2000","2000","loc");
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
-
 /*
 
 GET /jobs: Get all jobs
