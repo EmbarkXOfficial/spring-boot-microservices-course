14141fc9b7964768e995652207be5dcf3af48ab5
Faisal Memon <faisal.memon1991@gmail.com>
2023-07-11 12:21:05 +0530

    spring-micro-1290-company-and-review-relationship

--- firstjobapp/src/main/java/com/embarkx/firstjobapp/company/Company.java
+++ firstjobapp/src/main/java/com/embarkx/firstjobapp/company/Company.java
@@ -1,6 +1,7 @@
 package com.embarkx.firstjobapp.company;
 
 import com.embarkx.firstjobapp.job.Job;
+import com.embarkx.firstjobapp.review.Review;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
@@ -18,9 +19,18 @@ public class Company {
     @OneToMany(mappedBy = "company")
     private List<Job> jobs;
 
-    // private List<Review> reviews;
+    @OneToMany(mappedBy = "company")
+    private List<Review> reviews;
 
     public Company() {
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
     }
 
     public Long getId() {
