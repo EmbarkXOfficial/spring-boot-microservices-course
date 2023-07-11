c97515bc202512243239b97be36763b104ed71bb
Faisal Memon <faisal.memon1991@gmail.com>
2023-07-11 13:36:16 +0530

    spring-micro-1320-add-a-review-for-a-specific-company

--- firstjobapp/src/main/java/com/embarkx/firstjobapp/review/impl/ReviewServiceImpl.java
+++ firstjobapp/src/main/java/com/embarkx/firstjobapp/review/impl/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.embarkx.firstjobapp.review.impl;
 
+import com.embarkx.firstjobapp.company.Company;
+import com.embarkx.firstjobapp.company.CompanyService;
 import com.embarkx.firstjobapp.review.Review;
 import com.embarkx.firstjobapp.review.ReviewRepository;
 import com.embarkx.firstjobapp.review.ReviewService;
@@ -9,15 +11,30 @@ import java.util.List;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
+    private final CompanyService companyService;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository,
+                             CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findByCompanyId(companyId);
         return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if (company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
